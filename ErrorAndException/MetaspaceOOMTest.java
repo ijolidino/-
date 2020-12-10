2d4ca55ecfce74d5eca47337078230cff5536b3d
ijolidino <ijunido@gmail.com>
2020-12-10 20:29:52 +0800

    垃圾回收与默认

--- ErrorAndException/MetaspaceOOMTest.java
+++ ErrorAndException/MetaspaceOOMTest.java
@@ -1,30 +1,30 @@
-package ErrorAndException;
-import java.lang.reflect.Method;
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-public class MetaspaceOOMTest {
-    static class OOMTest{}
-
-    public static void main(String[] args) {
-        int i=0;//模拟计数多少次后发生异常
-        try {
-            while (true){
-                i++;
-                Enhancer enhancer = new Enhancer();
-                enhancer.setSuperclass(OOMTest.class);
-                enhancer.setUseCache(false);
-                enhancer.setCallback(new MethodInterceptor(){
-                    @Override
-                    public Object intercept(Object o,Method method,Object[] objects,MethodProxy methodProxy)throws Throwable{
-                        return methodProxy.invokeSuper(o,args);
-                    }
-
-                });
-            }
-        } catch (Exception e) {
-            System.out.println("*********************多少次后发生了异常"+i);
-            e.printStackTrace();
-        }
-    }
-}
+//package ErrorAndException;
+//import java.lang.reflect.Method;
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
+//public class MetaspaceOOMTest {
+//    static class OOMTest{}
+//
+//    public static void main(String[] args) {
+//        int i=0;//模拟计数多少次后发生异常
+//        try {
+//            while (true){
+//                i++;
+//                Enhancer enhancer = new Enhancer();
+//                enhancer.setSuperclass(OOMTest.class);
+//                enhancer.setUseCache(false);
+//                enhancer.setCallback(new MethodInterceptor(){
+//                    @Override
+//                    public Object intercept(Object o,Method method,Object[] objects,MethodProxy methodProxy)throws Throwable{
+//                        return methodProxy.invokeSuper(o,args);
+//                    }
+//
+//                });
+//            }
+//        } catch (Exception e) {
+//            System.out.println("*********************多少次后发生了异常"+i);
+//            e.printStackTrace();
+//        }
+//    }
+//}
---
+++ ErrorAndException/helloGC.java
@@ -0,0 +1,7 @@
+package ErrorAndException;
+
+public class helloGC {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
