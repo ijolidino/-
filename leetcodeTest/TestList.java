68506f466c1d4d527970bd56eb2c07dec7eacc43
ijolidino <ijunido@gmail.com>
2020-12-16 16:18:19 +0800

    list的concurrentModificationException测试

---
+++ leetcodeTest/TestList.java
@@ -0,0 +1,28 @@
+package leetcodeTest;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Create by Fuwen on 2020/12/16
+ */
+public class TestList {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        System.out.println("遍历修改前"+list.size());
+        list.get(0);
+        Iterator<String> iterator=list.iterator();
+
+        while(iterator.hasNext()){
+            String item=iterator.next();
+            if(item=="1"){
+                iterator.remove();
+            }
+            System.out.println(iterator.next());
+        }
+        System.out.println("遍历修改后"+list.size());
+    }
+}
