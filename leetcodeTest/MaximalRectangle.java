f091e61857f32e5ce464ac462a1953e7b9bb5a43
ijolidino <ijunido@gmail.com>
2020-12-26 13:29:31 +0800

    最大矩形

--- leetcodeTest/MaximalRectangle.java
+++ leetcodeTest/MaximalRectangle.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 /**
  * Create by Fuwen on 2020/12/26
- * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。v
  */
 public class MaximalRectangle {
         public int maximalRectangle(char[][] matrix) {
