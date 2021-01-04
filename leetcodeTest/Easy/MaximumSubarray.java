a253874f5af24b3cc9795c95883b8845d6245b39
ijolidino <ijunido@gmail.com>
2021-01-04 09:51:25 +0800

    最大子序列和

---
+++ leetcodeTest/Easy/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package leetcodeTest.Easy;
+
+/**
+ * Create by Fuwen on 2021/1/4
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+public class MaximumSubarray {
+        public int maxSubArray(int[] nums) {
+            int pre = 0, maxAns = nums[0];
+            for (int x : nums) {
+                pre = Math.max(pre + x, x);
+                maxAns = Math.max(maxAns, pre);
+            }
+            return maxAns;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(new MaximumSubarray().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+}
