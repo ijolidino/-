cb66d6af271cc308e358188d9d75eecaea12a0dd
ijolidino <ijunido@gmail.com>
2021-01-08 19:23:15 +0800

    旋转数组

---
+++ leetcodeTest/middleHart/RotateArray.java
@@ -0,0 +1,27 @@
+package leetcodeTest.middleHart;
+
+/**
+ * Create by Fuwen on 2021/1/8
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int count = gcd(k, n);
+        for (int start = 0; start < count; ++start) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start != current);
+        }
+    }
+
+    public int gcd(int x, int y) {
+        return y > 0 ? gcd(y, x % y) : x;
+    }
+}
