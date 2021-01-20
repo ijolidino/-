e82d0fa3ae9b39c8c288080d89db7f22d3e512c3
ijolidino <ijunido@gmail.com>
2021-01-20 10:14:55 +0800

    重塑矩阵

---
+++ leetcodeTest/Easy/matrixReshape.java
@@ -0,0 +1,65 @@
+package leetcodeTest.Easy;
+
+import java.util.ArrayDeque;
+
+/**
+ * Create by Fuwen on 2021/1/20
+ * 566. 重塑矩阵
+ * 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+ *
+ * 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+ *
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+ *
+ * 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 1, c = 4
+ * 输出:
+ * [[1,2,3,4]]
+ * 解释:
+ * 行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+ * 示例 2:
+ *
+ * 输入:
+ * nums =
+ * [[1,2],
+ *  [3,4]]
+ * r = 2, c = 4
+ * 输出:
+ * [[1,2],
+ *  [3,4]]
+ * 解释:
+ * 没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+ */
+public class matrixReshape {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums[0].length*nums.length!=r*c){
+            return nums;
+        }
+        int[][] ints = new int[r][c];
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                deque.addLast(nums[i][j]);
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (!deque.isEmpty()){
+                    ints[i][j]=deque.pollFirst();
+                }
+            }
+        }
+        return ints;
+    }
+
+    public static void main(String[] args) {
+        new matrixReshape().matrixReshape(new int[][]{{1,2,3,4}},2,2);
+    }
+}
