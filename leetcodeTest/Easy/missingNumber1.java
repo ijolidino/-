4eab67593dc9bebc54e62da0ece20797ec4d687a
ijolidino <ijunido@gmail.com>
2021-01-25 09:45:46 +0800

    丢失的数字

---
+++ leetcodeTest/Easy/missingNumber1.java
@@ -0,0 +1,55 @@
+package leetcodeTest.Easy;
+
+import javafx.collections.transformation.SortedList;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Create by Fuwen on 2021/1/25
+ * 268. 丢失的数字
+ * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你能否实现线性时间复杂度、仅使用额外常数空间的算法解决此问题?
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,0,1]
+ * 输出：2
+ * 解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 2：
+ *
+ * 输入：nums = [0,1]
+ * 输出：2
+ * 解释：n = 2，因为有 2 个数字，所以所有的数字都在范围 [0,2] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 3：
+ *
+ * 输入：nums = [9,6,4,2,3,5,7,0,1]
+ * 输出：8
+ * 解释：n = 9，因为有 9 个数字，所以所有的数字都在范围 [0,9] 内。8 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 4：
+ *
+ * 输入：nums = [0]
+ * 输出：1
+ * 解释：n = 1，因为有 1 个数字，所以所有的数字都在范围 [0,1] 内。1 是丢失的数字，因为它没有出现在 nums 中。
+ */
+public class missingNumber1 {
+    public int missingNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int num : nums) numSet.add(num);
+
+        int expectedNumCount = nums.length + 1;
+        for (int number = 0; number < expectedNumCount; number++) {
+            if (!numSet.contains(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+}
