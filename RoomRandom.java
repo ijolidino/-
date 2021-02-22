69c17251fbbe55fc9dd2fe57ad37fd857ff19a80
ijolidino <ijunido@gmail.com>
2021-02-22 10:04:48 +0800

    update .gitignore

---
+++ RoomRandom.java
@@ -0,0 +1,13 @@
+
+/**
+ * @program: Testcode
+ * @description: 户型选型摇号
+ * @author: Fuwen
+ * @create: 2021-02-20 21:12
+ **/
+public class RoomRandom {
+    public static void main(String[] args) {
+        int random = (int)(Math.random()*1000000);
+        System.out.println(random);
+    }
+}
